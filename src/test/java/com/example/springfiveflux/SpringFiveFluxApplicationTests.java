02e16583ac6f0a6f3a12b47be71be5af6a515542
Dawid Kubicki <d.kubicki@partner.samsung.com>
2017-11-30 12:20:12 +0100

    init

---
+++ src/test/java/com/example/springfiveflux/SpringFiveFluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springfiveflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFiveFluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
